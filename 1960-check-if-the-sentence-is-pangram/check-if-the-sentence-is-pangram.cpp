2c262b2d922243872e5683a5e884da287f5d7210
shubham chaudhary <142807471+shubhlc194@users.noreply.github.com>
2025-05-31 15:43:14 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (17.65%) - LeetSync

--- 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.cpp
+++ 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.cpp
@@ -3,9 +3,9 @@ public:
     bool checkIfPangram(string sentence) {
        unordered_set<char> letters;
         for (char ch : sentence) {
-            if (isalpha(ch)) {
+        
                 letters.insert(ch);
-            }
+            
         }
         return letters.size() == 26;
     }
