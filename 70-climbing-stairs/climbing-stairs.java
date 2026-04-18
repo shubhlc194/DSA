ea39e28bc5347afc98390f5461841bee71fefd1b
shubham choudhary <142807471+shubhlc194@users.noreply.github.com>
2026-04-18 12:06:58 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (76.00%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -6,9 +6,7 @@ class Solution {
 
     public int solve(int n, int[] dp) {
         if(n <= 1) return 1;
-
         if(dp[n] != 0) return dp[n];
-
         return dp[n] = solve(n-1, dp) + solve(n-2, dp);
     }
 }
