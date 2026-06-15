1f324891ff0428333ef18d5046959032558353e4
shubham choudhary <142807471+shubhlc194@users.noreply.github.com>
2026-06-15 09:44:48 +0530

    Time: 2 ms (97.02%) | Memory: 43.4 MB (62.11%) - LeetSync

--- 1297-maximum-number-of-balloons/maximum-number-of-balloons.java
+++ 1297-maximum-number-of-balloons/maximum-number-of-balloons.java
@@ -1,36 +1,14 @@
 class Solution {
     public int maxNumberOfBalloons(String text) {
-        int ans = 0;
-
-        while (true) {
-            HashMap<Character, Integer> mpp = new HashMap<>();
-            mpp.put('b', 1);
-            mpp.put('a', 1);
-            mpp.put('l', 2);
-            mpp.put('o', 2);
-            mpp.put('n', 1);
-            for (char ch : text.toCharArray()) {
-                if (mpp.containsKey(ch)) {
-                    mpp.put(ch, mpp.get(ch) - 1);
-                    if (mpp.get(ch) == 0) {
-                        mpp.remove(ch);
-                    }
-                }
-            }
-
-            if (mpp.size() == 0) {
-                ans++;
-                text = text.replaceFirst("b", "");
-                text = text.replaceFirst("a", "");
-                text = text.replaceFirst("l", "");
-                text = text.replaceFirst("l", "");
-                text = text.replaceFirst("o", "");
-                text = text.replaceFirst("o", "");
-                text = text.replaceFirst("n", "");
-            } else {
-                break;
-            }
+        int[] freq = new int[26];
+        for (char ch : text.toCharArray()) {
+            freq[ch - 'a']++;
         }
+        int ans = freq['b' - 'a'];
+        ans = Math.min(ans, freq['a' - 'a']);
+        ans = Math.min(ans, freq['l' - 'a'] / 2);
+        ans = Math.min(ans, freq['o' - 'a'] / 2);
+        ans = Math.min(ans, freq['n' - 'a']);
 
         return ans;
     }
